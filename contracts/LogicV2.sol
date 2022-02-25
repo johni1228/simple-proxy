8c6dd1105bec694f97b65cf0f9b47f5d9e257650
johni1228 <akirus1228@gmail.com>
2022-02-25 05:07:15 -0800

    feat: Init project

--- contracts/LogicV2.sol
+++ contracts/LogicV2.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.8.4;
+pragma solidity 0.8.4;
 
 contract LogicV2 {
     bool initialized;
--- contracts/Proxy.sol
+++ contracts/Proxy.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.8.4;
+pragma solidity 0.8.4;
 
 library StorageSlot {
     function getAddressAt(bytes32 slot) internal view returns (address a) {
